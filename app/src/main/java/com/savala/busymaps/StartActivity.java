2569f64d2059add4825d6b967fec348939c41f32
timothylidede <75303411+timothylidede@users.noreply.github.com>
2021-12-30 12:57:59 +0300

    connecting maps api to the application

--- app/src/main/java/com/savala/busymaps/StartActivity.java
+++ app/src/main/java/com/savala/busymaps/StartActivity.java
@@ -3,9 +3,16 @@ package com.savala.busymaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class StartActivity extends AppCompatActivity {
+
+    private static final String TAG = "StartActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,11 @@ public class StartActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.splash_screen);
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK(): checking google services version");
+
+        int available = GoogleApiAvailability
     }
 }
