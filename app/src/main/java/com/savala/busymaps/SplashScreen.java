ae80791185737a69c4a70cd56500a79b2cf9fba1
timothylidede <75303411+timothylidede@users.noreply.github.com>
2021-12-30 16:49:56 +0300

    finished initial beginning classes and xml files

--- app/src/main/java/com/savala/busymaps/SplashScreen.java
+++ app/src/main/java/com/savala/busymaps/SplashScreen.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1500;
+    //constants
+    private static int SPLASH_TIME_OUT = 3000;
+
+    //widgets
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,10 @@ public class SplashScreen extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.splash_screen);
+
+        //initialize widgets
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.VISIBLE);
 
         new Handler().postDelayed(new Runnable() {
             @Override
--- app/src/main/java/com/savala/busymaps/StartActivity.java
+++ app/src/main/java/com/savala/busymaps/StartActivity.java
@@ -2,10 +2,13 @@ package com.savala.busymaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
 public class StartActivity extends AppCompatActivity {
@@ -20,12 +23,36 @@ public class StartActivity extends AppCompatActivity {
         requestWindowFeature(getWindow().FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.activity_start);
+
+        if(isServicesOK()){
+            init();
+        }
     }
 
+    private void init(){
+
+    }
+
+    //method that checks whether the user's Play services is up to date
     public boolean isServicesOK(){
         Log.d(TAG, "isServicesOK(): checking google services version");
 
-        int available = GoogleApiAvailability
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(StartActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(StartActivity.this, available,
+                    ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You cannot make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
