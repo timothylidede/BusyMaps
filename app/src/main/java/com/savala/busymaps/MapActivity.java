2b7616a8280c9e871d594e36bab5b1eb59d37625
timothylidede <75303411+timothylidede@users.noreply.github.com>
2021-12-31 13:07:31 +0300

    setting up map permissions and initializations

--- app/src/main/java/com/savala/busymaps/MapActivity.java
+++ app/src/main/java/com/savala/busymaps/MapActivity.java
@@ -1,14 +1,27 @@
 package com.savala.busymaps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.WindowManager;
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapActivity extends AppCompatActivity {
 
     //constants
     private static final String TAG = "MapActivity";
+    private static final  String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final  String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final  int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    //vars
+    private Boolean mLocationPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +30,49 @@ public class MapActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_map);
+    }
+
+    private void initMap(){
+        SupportMapFragment
+    }
+
+    private void getLocationPermission(){
+        String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted = true;
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        mLocationPermissionGranted = false;
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    for(int i = 0; i < grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+
+                    //initialize our map
+                }
+            }
+        }
+
     }
 }
