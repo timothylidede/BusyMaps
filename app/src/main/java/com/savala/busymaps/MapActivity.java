982c0b91b7dccdc8c171790996ff14f5d36e173f
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-03 21:10:46 +0300

    hiding the centering icon

--- app/src/main/java/com/savala/busymaps/MapActivity.java
+++ app/src/main/java/com/savala/busymaps/MapActivity.java
@@ -46,6 +46,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 return;
             }
             mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
         }
     }
 
